a8d3ef540df7de938e4ce18fddb3ca1fe4c7cab8
Polikis <contacto.mail99@gmail.com>
2012-05-09 21:44:30 +0000

    Small Bug Fix

--- QEvent.js
+++ QEvent.js
@@ -1,20 +1,20 @@
 (function () {
-    var QItem = function (Query) {
-            if (!!document.querySelectorAll(Query)[1]) {
-                return document.querySelectorAll(Query);
-            } else {
-                return document.querySelector(Query);
-            }
-        };
+    var QItem = function(Query) {
+        if (!!document.querySelectorAll(Query)[1]) {
+            return document.querySelectorAll(Query);
+        } else {
+            return document.querySelector(Query);
+        }
+    };
     window.$ = QItem;
     
     HTMLElement.prototype.addEvent = function (type,callback,fase) {
 		if (!!document.addEventListener) {
-	    	this.addEventListener(type, callback, fase || false);
-	    } else if (!!document.attachEvent) {
-	        this.attachEvent('on' + type, function() { 
-	        	return callback.call(elem, window.event); 
-	    	});
+            this.addEventListener(type, callback, fase || false);
+        } else if (!!document.attachEvent) {
+            this.attachEvent('on' + type, function() { 
+                return callback.call(this, window.event); 
+            });
 		}
 	};
 })();
